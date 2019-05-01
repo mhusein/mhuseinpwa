0503244f5436f479cbe2e48cde735e525b18c8fa
mhuseinlab <mhuseinlab@gmail.com>
2019-05-01 07:51:57 +0700

    memperbaharui web push

--- server_push/main.js
+++ server_push/main.js
@@ -1,12 +1,12 @@
 var webPush = require('web-push');
 var pushSubscription = {
-    "endpoint": "https://android.googleapis.com/gcm/send/eDMtUo7PdbI:APA91bGepavQz9-4Wop5nLg487yplCatHnug99heLzi0A3vsHpb4DW52RjCNAXL0MMM-Rt2c7oAZonSDkoX9LGdxkxVh1HstiSwnotGbHdddr2lEtQM8THrpVBlFsudeXHWybfbhfcvp",
+    "endpoint": "https://android.googleapis.com/gcm/send/dz_gj4uiUys:APA91bFo7SRmdlAo-PdOfNnIvbVWFFLhxNtHCkASNaA97BqGO8siRJbSzR8A6p6O96NI1R0ZgPSqNu2vvOhxfGliI_sL89CEhis7tOfygZ3sWqAc-vsmZpwdaSJrlPB0n8edg3KXsfMN",
     "keys": {
-        "p256dh": "BMbTRgcoDIxa0eD4zLU3SwUFopH50iyYV+aorjAkKAJ+yU38B250H4y4sM788LJYydAAWSBYgjJTDaGf4wpREiw=",
-        "auth": "SQm+Pm3k2Urp0dIDPoRE7g=="
+        "p256dh": "BEgFln/qIKF3KAF0RIpbcD+7oodmxNgQMmIAQYhiYBhUNc6jJC+I0E6wvSA6Ka0WDUfIzQ6IVWis6HRs2yKr9/A=",
+        "auth": "lKoWo0F1k+3ROPbnaT3Ikw=="
     }
 };
-var payload = 'Pak iki aku wes iso ngirim nganggo payload!';
+var payload = 'Mbak iki aku wes iso ngirim nganggo payload!';
 var options = {
     gcmAPIKey: 'AAAAIt_PeEA:APA91bEpyxsv9JzrdA_C3QW_XtC5KHSK0MqMyGVmYxLccdkDIxn7_eq6ByezdD6RMzPzXAOKZ5dTgDbgl2ajdoQbEgIPdfq8B_ptXQw6a1gvWcRUh7L3bjEfGNPWPGgiwpkjWR8dT3Tv',
     TTL: 60
